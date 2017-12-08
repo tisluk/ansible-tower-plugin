b879a237916e4f872dfebf5d256ca263d6a9113e
John Westcott IV <john.westcott.iv@redhat.com>
2017-12-07 19:54:20 -0500

    Ammend to last commit

--- src/main/java/org/jenkinsci/plugins/ansible_tower/AnsibleTowerRunner.java
+++ src/main/java/org/jenkinsci/plugins/ansible_tower/AnsibleTowerRunner.java
@@ -3,6 +3,7 @@ package org.jenkinsci.plugins.ansible_tower;
 import hudson.EnvVars;
 import hudson.model.AbstractBuild;
 import hudson.model.Result;
+import net.sf.json.JSONObject;
 import org.jenkinsci.plugins.ansible_tower.util.TowerConnector;
 import org.jenkinsci.plugins.ansible_tower.util.TowerInstallation;
 
@@ -67,13 +68,45 @@ public class AnsibleTowerRunner {
             }
         }
 
+        // Get the job template.
+        JSONObject template = null;
+        try {
+            template = myTowerConnection.getJobTemplate(jobTemplate, templateType);
+        } catch(AnsibleTowerException e) {
+            logger.println("ERROR: Unable to lookup job template " + e.getMessage());
+            return false;
+        }
+
+
+        if(expandedExtraVars != null && template.containsKey("ask_variables_on_launch") && template.getBoolean("ask_variables_on_launch")) {
+            logger.println("[WARNING]: Extra variables defined but prompt for variables on launch is not set in tower job");
+        }
+        if(expandedLimit != null && template.containsKey("ask_limit_on_launch") && template.getBoolean("ask_limit_on_launch")) {
+            logger.println("[WARNING]: Limit defined but prompt for limit on launch is not set in tower job");
+        }
+        if(expandedJobTags != null && template.containsKey("ask_tags_on_launch") && template.getBoolean("ask_tags_on_launch")) {
+            logger.println("[WARNING]: Job Tags defined but prompt for tags on launch is not set in tower job");
+        }
+        if(expandedInventory != null && template.containsKey("ask_inventory_on_launch") && template.getBoolean("ask_inventory_on_launch")) {
+            logger.println("[WARNING]: Inventory defined but prompt for inventory on launch is not set in tower job");
+        }
+        if(expandedCredential != null && template.containsKey("ask_credential_on_launch") && template.getBoolean("ask_credential_on_launch")) {
+            logger.println("[WARNING]: Credential defined but prompt for credential on launch is not set in tower job");
+        }
+        // Here are some more options we may want to use someday
+        //    "ask_diff_mode_on_launch": false,
+        //    "ask_skip_tags_on_launch": false,
+        //    "ask_job_type_on_launch": false,
+        //    "ask_verbosity_on_launch": false,
+
+
         if(verbose) {
             logger.println("Requesting tower to run " + templateType + " template " + jobTemplate);
         }
         int myJobID;
         try {
             System.out.println("The template type is "+ templateType);
-            myJobID = myTowerConnection.submitTemplate(expandedJobTemplate, expandedExtraVars, expandedLimit, expandedJobTags, expandedInventory, expandedCredential, templateType);
+            myJobID = myTowerConnection.submitTemplate(template.getInt("id"), expandedExtraVars, expandedLimit, expandedJobTags, expandedInventory, expandedCredential, templateType);
         } catch (AnsibleTowerException e) {
             logger.println("ERROR: Unable to request job template invocation " + e.getMessage());
             return false;
